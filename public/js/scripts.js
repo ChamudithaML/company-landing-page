26aebe1c0c06b8d05dcedb693af92ddedd4bfa0e
ChamudithaML <lilan.2019660@iit.ac.lk>
2024-10-20 09:21:49 +0530

    add navbar and header; add hero section

---
+++ public/js/scripts.js
@@ -0,0 +1,19 @@
+// Language Switcher
+const languageDropdown = document.getElementById('language-dropdown');
+
+languageDropdown.addEventListener('change', (event) => {
+    const selectedLanguage = event.target.value;
+    if (selectedLanguage === 'en') {
+        console.log('Language switched to English');
+    } else if (selectedLanguage === 'es') {
+        console.log('Language switched to Spanish');
+    }
+});
+
+// Hamburger Menu
+const navbarToggler = document.querySelector('.navbar-toggler');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+
+navbarToggler.addEventListener('click', () => {
+    console.log("Clicked")
+});
